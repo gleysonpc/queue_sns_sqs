8a4d3eb649074d20913018b1aeff2a67e4b70411
Gleyson <gleyson_10@hotmail.com>
2023-05-07 16:07:06 -0300

    add .env support

--- hello.py
+++ hello.py
@@ -6,6 +6,6 @@ def run(event, context):
     print('######## hello ########')
     print({
         'arn': os.environ['NEW_USERS_TOPIC_ARN'],
-        'topic': os.environ['NEW_USERS_TOPIC']
+        'db_user': os.environ['DB_USER']
     })
     return {"statusCode": 200, "body": json.dumps({'message': 'hi there!'})}
